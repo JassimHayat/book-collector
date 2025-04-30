7356d90a1aa59ec2d330fe537ca8fa48e7d2b599
Jassim Hayat <jassim.hayat@gmail.com>
2025-04-30 20:19:25 +0300

    Add Authentication

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('books/', views.book_index, name='book-index'),
     path('books/<int:book_id>/', views.book_detail, name='book-detail'),
     path('books/create/', views.BookCreate.as_view(), name='book-create'),
     path('Books/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
     path('Books/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
-    
+    path('accounts/signup/', views.signup, name='signup'),
+       
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render # main_app/views.py
+from django.shortcuts import render, redirect # main_app/views.py
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Book
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
+
+
+
+
+
+class Home(LoginView):
+    template_name = 'home.html'
 
 
 class BookCreate(CreateView):
     model = Book
     fields = ['bookTitle','description', 'genre', 'releaseDate']
     # success_url = '/books/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+def form_valid(self, form):
+        # Assign the logged in user (self.request.user)
+        form.instance.user = self.request.user  # form.instance is the book
+        # Let the CreateView do its job as usual
+        return super().form_valid(form)
 
 class BookUpdate(UpdateView):
     model = Book
@@ -21,17 +42,39 @@ class BookDelete(DeleteView):
 
 
 # Define the home view function
-def home(request):
-    # Send a simple HTML response
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+
 
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def book_index(request):
-    books = Book.objects.all()
-    return render(request, 'books/index.html', {'books': books})
+    books = Book.objects.filter(user=request.user)
+    return render(request, 'books/index.html', { 'books': books })
 
+@login_required
 def book_detail(request, book_id):
     book = Book.objects.get(id=book_id)
     return render(request, 'books/detail.html', {'book': book})
+
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in
+            login(request, user)
+            return redirect('book-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
+    
